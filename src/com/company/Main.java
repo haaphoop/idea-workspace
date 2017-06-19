be61791a0e93670340e38a2ec85f0f050dd6a71c
daishuli <314003129@qq.com>
2017-06-19 19:29:06 +0800

    fdsaf

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    String s = "";
+    // 字符串可以用带引号的文本初始化
+    String t = new String("asdf");
+    System.out.println(t);
+
+    }
+}
---
+++ src/com/company/haaphoop/thinkingInJava/Chapter2.java
@@ -0,0 +1,17 @@
+package com.company.haaphoop.thinkingInJava;
+
+/**
+ * Created by daishuli on 2017/6/19.
+ */
+public class Chapter2 {
+    public static void main(String[] args) {
+        // write your code here
+        String s = "";
+        // 字符串可以用带引号的文本初始化
+        String t = new String("asdf");
+        System.out.println(t);
+        // 2.2 必须由你创建所有对象
+
+
+    }
+}
