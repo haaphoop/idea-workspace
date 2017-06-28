cd979408387c5a6f1ead7225335e16ed21a49560
daishuli <314003129@qq.com>
2017-06-28 09:31:51 +0800

    study

---
+++ src/com/company/haaphoop/thinkingInJava/chapter9/Instrument.java
@@ -0,0 +1,18 @@
+package com.company.haaphoop.thinkingInJava.chapter9;
+
+/**
+ * Created by daishuli on 2017/6/27.
+ */
+public abstract class Instrument {
+    private int i; // Storage allocated for each
+    public abstract void play(Note note);
+    public String what() {return "Instrument";}
+    public abstract void adjust();
+}
+
+class Wind extends Instrument {
+    @Override
+    public void play(Note note) {
+        System.out.println("Wind.play()" + note);
+    }
+}
