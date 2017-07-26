b2d8a73f6aa3a08a381248b8cd3b2dd04a29460b
haaphoop <314003129@qq.com>
2017-07-26 20:31:55 +0800

    home

--- src/com/company/haaphoop/thinkingInJava/chapter14/Circle.java
+++ src/com/company/haaphoop/thinkingInJava/chapter14/Circle.java
@@ -8,7 +8,7 @@ import java.util.List;
  */
 
 
-public class Circle extends Shape{
+public class Circle extends Shape{ //fdsaf
     @Override
     public String toString() {
         return "Circle{}";
