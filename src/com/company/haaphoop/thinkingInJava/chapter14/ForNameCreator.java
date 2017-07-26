2720ffd98075c1cd8f10247346c598dad335901f
haaphoop <314003129@qq.com>
2017-07-26 22:21:29 +0800

    home

--- src/com/company/haaphoop/thinkingInJava/chapter14/ForNameCreator.java
+++ src/com/company/haaphoop/thinkingInJava/chapter14/ForNameCreator.java
@@ -5,7 +5,107 @@ import java.util.*;
 /**
  * Created by daishuli on 2017/7/26.
  */
-public class ForNameCreator {
+public class ForNameCreator extends PetCreator{
+    private static List<Class<? extends  Pet>> types =
+            new ArrayList<Class<? extends Pet>>();
+    private static String[] typeNames = {
+            "com.company.haaphoop.thinkingInJava.chapter14.Mutt",
+            "com.company.haaphoop.thinkingInJava.chapter14.Pug",
+            "com.company.haaphoop.thinkingInJava.chapter14.EgyptianMau",
+            "com.company.haaphoop.thinkingInJava.chapter14.Manx",
+            "com.company.haaphoop.thinkingInJava.chapter14.Cymric",
+            "com.company.haaphoop.thinkingInJava.chapter14.Mouse",
+            "com.company.haaphoop.thinkingInJava.chapter14.Pet",
+            "com.company.haaphoop.thinkingInJava.chapter14.Hamster"
+    };
+
+    @SuppressWarnings("unchecked")
+    private static void loader() {
+        try {
+            for (String name : typeNames) {
+                types.add((Class<? extends Pet>) Class.forName(name));
+            }
+        }catch (ClassNotFoundException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    static {
+        loader();
+    }
+
+    public List<Class<? extends Pet>> types(){return types;};
+}
+
+class PetCount {
+    static class PetCounter extends HashMap<String, Integer> {
+        public void count(String type) {
+            Integer quantity = get(type);
+            if (quantity == null) {
+                put(type, 1);
+            } else {
+                put(type, quantity + 1);
+            }
+        }
+    }
+
+    public static void countPets(PetCreator creator) {
+        PetCounter count = new PetCounter();
+        for (Pet pet : creator.createArray(20)) {
+
+            System.out.println(pet.getClass().getSimpleName() + " ");
+            if (pet instanceof Pet) {
+                count.count("Pet");
+            }
+
+            if (pet instanceof Dog) {
+                count.count("Dog");
+            }
+
+            if (pet instanceof Mutt) {
+                count.count("Mutt");
+            }
+
+            if (pet instanceof Pug) {
+                count.count("Pug");
+            }
+
+            if (pet instanceof Cat) {
+                count.count("Cat");
+            }
+
+            if (pet instanceof Manx) {
+                count.count("Manx");
+            }
+
+            if (pet instanceof Cymric) {
+                count.count("Cymric");
+            }
+
+            if (pet instanceof Rodent) {
+                count.count("Rodent");
+            }
+
+            if (pet instanceof Rat) {
+                count.count("Rat");
+            }
+
+            if (pet instanceof Mouse) {
+                count.count("Mouse");
+            }
+
+            if (pet instanceof Hamster) {
+                count.count("Hamster");
+            }
+        }
+
+        System.out.println();
+        System.out.println(count);
+    }
+
+    public static void main(String[] args) {
+        countPets(new ForNameCreator());
+    }
 }
 
 class Individual {
