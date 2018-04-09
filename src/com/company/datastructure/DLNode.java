02961c7e990aa09d26757510a9c79d8cc5eaa4ed
haaphoop <314003129@qq.com>
2018-04-09 23:55:59 +0800

    home

---
+++ src/com/company/datastructure/DLNode.java
@@ -0,0 +1,52 @@
+package com.company.datastructure;
+
+public class DLNode implements Node {
+
+    private Object element;
+    private DLNode pre;
+    private DLNode next;
+
+    public Object getElement() {
+        return element;
+    }
+
+    public void setElement(Object element) {
+        this.element = element;
+    }
+
+    public DLNode getPre() {
+        return pre;
+    }
+
+    public void setPre(DLNode pre) {
+        this.pre = pre;
+    }
+
+    public DLNode getNext() {
+        return next;
+    }
+
+    public void setNext(DLNode next) {
+        this.next = next;
+    }
+
+    public DLNode() {
+        this(null, null, null);
+    }
+
+    public DLNode(Object element, DLNode pre, DLNode next) {
+        this.element = element;
+        this.pre = pre;
+        this.next = next;
+    }
+
+    @Override
+    public Object getData() {
+        return this.element;
+    }
+
+    @Override
+    public void setData(Object obj) {
+        this.element = obj;
+    }
+}
---
+++ src/com/company/datastructure/Test.java
@@ -0,0 +1,94 @@
+package com.company.datastructure;
+
+public class Test {
+    public ListNode delete(ListNode pHead) {
+        /**ListNode p = pHead;
+        pHead = null;
+        while (p != null) {
+            if (p.next != null) {
+                if (p.next.val != p.val) {
+                    add(pHead, p.val);
+                    p = p.next;
+                } else {
+                    ListNode temp = p.next.next;
+                    while (temp != null) {
+                        if (p.val == temp.val) {
+                            temp = temp.next;
+                        } else {
+                            p = temp;
+                            temp = null;
+                        }
+                    }
+                }
+            } else {
+                add(pHead, p.val);
+                p = p.next;
+            }
+        }**/
+        ListNode p = pHead;
+        while (p != null) {
+            if (p.next != null) {
+                if (p.next.val != p.val) {
+                    add(pHead, p.val);
+                    p = p.next;
+                } else {
+                    ListNode temp = p.next.next;
+                    while (temp != null) {
+                        if (p.val == temp.val) {
+                            temp = temp.next;
+                        } else {
+                            p = temp;
+                            temp = null;
+                        }
+                    }
+                }
+            } else {
+                add(pHead, p.val);
+                p = p.next;
+            }
+        }
+        return pHead;
+    }
+
+    private void add(ListNode pHead, int val) {
+        if (pHead == null) {
+            pHead = new ListNode(val);
+        } else {
+            ListNode pp = pHead;
+            ListNode tmp = new ListNode(val);
+            while (pp.next != null) {
+                pp = pp.next;
+            }
+            pp.next = tmp;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+
+        ListNode qq = head;
+        head.next = new ListNode(2);
+        qq.next = new ListNode(5);
+        /*
+        head.next.next = new ListNode(1);
+        head.next.next.next = new ListNode(1);
+        head.next.next.next.next = new ListNode(1);
+        head.next.next.next.next.next = new ListNode(1);
+        head.next.next.next.next.next.next = new ListNode(2);*/
+        Test test = new Test();
+        //ListNode tt = test.delete(head);
+        while (head != null) {
+            System.out.println(head.val);
+            head = head.next;
+        }
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next = null;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+}
