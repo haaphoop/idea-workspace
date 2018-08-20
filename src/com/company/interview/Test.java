21f04638e77f99a09c2ab6d15eb00f9ad280cd95
haaphoop <314003129@qq.com>
2018-08-20 22:44:37 +0800

    home

--- src/com/company/interview/Test.java
+++ src/com/company/interview/Test.java
@@ -2,16 +2,8 @@ package com.company.interview;
 
 public class Test {
     public static void main(String[] args) {
-        int i=10;
-        if (i++ == 11 && i++ == 12) {
-            System.out.println(i);
-        }
-        System.out.println("i= "+ i);
-
-        int n =10;
-        if (n++ == 11 & n++== 12){
-            System.out.println(n);
-        }
-        System.out.println("n= "+ n);
+        String str = "ddd";
+        String str1 = new String("ddd").intern();
+        System.out.println(str == str1);
     }
 }
